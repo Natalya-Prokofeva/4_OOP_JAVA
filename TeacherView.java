9fe2032c3cc8a3e6b9df338f455f9f135863992e
Natalya <159589443+Natalya-Prokofeva@users.noreply.github.com>
2024-09-21 20:06:21 +0300

    Create TeacherView.java

---
+++ TeacherView.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.util.logging.Logger;
+
+public class TeacherView implements UserView<Teacher> {
+
+    Logger logger = Logger.getLogger(TeacherView.class.getName());
+
+    @Override
+    public void sendOnConsole(List<Teacher> teachers) {
+        for(Teacher teacher: teachers) {
+            logger.info(teacher.toString());
+        }
+    }
+}
