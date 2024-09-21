2b165cf25984c70884487f7a2b5f49e99c5f1f10
Natalya <159589443+Natalya-Prokofeva@users.noreply.github.com>
2024-09-21 20:07:13 +0300

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -1,1 +1,29 @@
+import java.time.LocalDate;
 
+public class Teacher extends User {
+    private Long teacherId;
+
+    public Teacher(String firstName, String secondName, String patronymic, LocalDate dateOfBirth, Long teacherId) {
+        super(firstName, secondName, patronymic, dateOfBirth);
+        this.teacherId = teacherId;
+    }
+
+    public Long getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(Long teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "teacherId='" + teacherId + '\'' +
+                ", firstName='" + super.getFirstName() + '\'' +
+                ", secondName='" + super.getSecondName() + '\'' +
+                ", patronymic='" + super.getPatronymic() + '\'' +
+                ", dateOfBirth=" + super.getDateOfBirth() +
+                '}';
+    }
+}
